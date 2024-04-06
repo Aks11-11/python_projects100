1ad5c75dd00e7a8e9ed42d13d7221da3d5724375
Aks11-11 <aksshatvashisht@gmail.com>
2024-04-06 14:41:15 +0530

    /

--- chess_board.py
+++ chess_board.py
@@ -8,7 +8,6 @@ X, Y = np.meshgrid(x, y)
 extent = np.min(x), np.max(x), np.min(y), np.max(y)
 z1 = np.add.outer(range(8), range(8)) % 2
 plt.imshow(z1, cmap="binary_r", interpolation="nearest", extent=extent, alpha=1)
-
 def chess(x, y):
 return (1 - x / 2 + x ** 5 + y ** 6) * np.exp(-(x ** 2 + y ** 2))
 z2 = chess(X, Y)
