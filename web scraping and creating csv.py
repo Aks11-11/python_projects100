701163d0769fade59079283f6e075fab858bce24
Aks11-11 <aksshatvashisht@gmail.com>
2024-06-15 18:02:36 +0530

    \

--- web scraping and creating csv.py
+++ web scraping and creating csv.py
@@ -12,7 +12,6 @@ containers = page_soup.findAll("div", { "class": "_3O0U0u"})
 print(len(containers))
 
 print(soup.prettify(containers[0]))
-
 container = containers[0]
 print(container.div.img["alt"])
 
