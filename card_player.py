fa0dece229ef34666c2c11e32a83fd34b1e4bf5f
Aks11-11 <aksshatvashisht@gmail.com>
2024-06-19 21:06:25 +0530

    \

--- card_player.py
+++ card_player.py
@@ -1,7 +1,6 @@
 import random
 cards = ["Diamonds", "Spades", "Hearts", "Clubs"]
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-
 def pick_a_card():
     card = random.choices(cards)
     rank = random.choices(ranks)
