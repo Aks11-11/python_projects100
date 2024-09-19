6993f3ef619970374cd1ee15be0960925cb86885
Aks11-11 <aksshatvashisht@gmail.com>
2024-09-19 22:50:12 +0530

    /

--- web scraping and creating csv.py
+++ web scraping and creating csv.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
-
 my_url="https://www.flipkart.com/search?q=samsung+mobiles&amp;sid=tyy%2C4io&amp;as=on&amp;as-show=on&amp;otracker=AS_QueryStore_HistoryAutoSuggest_0_2&amp;otracker1=AS_QueryStore_HistoryAutoSuggest_0_2&amp;as-pos=0&amp;as-type=HISTORY&amp;as-searchtext=sa"
 
 uClient = uReq(my_url)
