ab8641ac3b29ff2d7f3e780cc4ce41c5371ccb34
Aks11-11 <aksshatvashisht@gmail.com>
2024-11-05 20:36:34 +0530

    /

--- chess_board.py
+++ chess_board.py
@@ -10,7 +10,6 @@ extent = np.min(x), np.max(x), np.min(y), np.max(y)
 z1 = np.add.outer(range(8), range(8)) % 2
 plt.imshow(z1, cmap="binary_r", interpolation="nearest", extent=extent, alpha=1)
 def chess(x, y):
-
     return (1 - x / 2 + x ** 5 + y ** 6) * np.exp(-(x ** 2 + y ** 2))
 z2 = chess(X, Y)
 plt.imshow(z2, alpha=0.7, interpolation="bilinear", extent=extent)
